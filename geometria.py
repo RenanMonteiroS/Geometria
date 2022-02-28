43e36dea4cffa61dc15f58b0521772b025655952
Monteiros <renan.monteiro.de.souza@gmail.com>
2022-02-28 09:52:53 -0300

    Separação de arquivos

--- geometria.py
+++ geometria.py
@@ -1,93 +1,136 @@
-class Garrafa:
-    __slots__ = ['_capacidade', '_volume']
+class Ponto:
+    __slots__ = ['_x_coord', '_y_coord']
 
-    def __init__(self, capacidade=500, volume=0):
+    def __init__(self, x_coord, y_coord):
 
-        if not isinstance(capacidade, (int, float)) or not isinstance(volume, (int, float)):
+        if not isinstance(x_coord, int) or not isinstance(y_coord, int):
             raise TypeError("O valor é inválido! Tente novamente com um número válido. Ex.: 100")
-        elif capacidade < 0 or volume < 0:
-            raise ValueError("O valor é negativo. Tente novamente com um número positivo. Ex.: 100")
         else:
-            self._capacidade = capacidade
-            self._volume = volume
+            self._x_coord = x_coord
+            self._y_coord = y_coord
+
+    @property
+    def x_coord(self):
+        return self._x_coord
+
+    @property
+    def y_coord(self):
+        return self._y_coord
+
+    @x_coord.setter
+    def x_coord(self, valor):
+        self._x_coord = valor
+
+    @y_coord.setter
+    def y_coord(self, valor):
+        self._y_coord = valor
 
     def __str__(self):
-        return f'Capacidade:{self._capacidade}ml, Volume:{self._volume}ml'
+        return f'Ponto:({self.x_coord}, {self.y_coord})'
+
+    def distancia(self, outro_ponto):
+        distancia = (((outro_ponto.x_coord - self.x_coord) ** 2) + ((outro_ponto.y_coord - self.y_coord) ** 2)) ** 0.5
+        return distancia
+
+    def resetar(self):
+        self.x_coord = 0
+        self.y_coord = 0
+
+class Circulo(Ponto):
+    __slots__ = ['_raio']
+
+    def __init__(self, x_coord, y_coord, raio):
+        super().__init__(x_coord, y_coord)
+        if not isinstance(raio, (int, float)):
+            raise TypeError("O valor é inválido! Tente novamente com um número válido. Ex.: 100")
+        else:
+            self._raio = raio
 
     @property
-    def capacidade(self):
-        return self._capacidade
+    def raio(self):
+        return self._raio
+
+    @raio.setter
+    def raio(self, valor):
+        self._raio = valor
+
+    def __str__(self):
+        return f'Centro:({self.x_coord}, {self.y_coord}. Raio: {self.raio})'
+
+    def area(self):
+        area = 3.14 * (self._raio ** 2)
+        return area
+
+class Cilindro(Circulo):
+    __slots__ = ['_altura']
+
+    def __init__(self, x_coord, y_coord, raio, altura):
+        super().__init__(x_coord, y_coord, raio)
+        if not isinstance(altura, (int, float)):
+            raise TypeError("O valor é inválido! Tente novamente com um número válido. Ex.: 100")
+        else:
+            self._altura = altura
 
     @property
+    def altura(self):
+        return self._altura
+
+    @altura.setter
+    def altura(self, valor):
+        self._altura = valor
+
+    def area(self):
+        area = (2 * 3.14 * self.raio * self.altura) + ((3.14 * (self.raio ** 2)) * 2)
+        return area
+
     def volume(self):
-        return self._volume
+        volume = (self.raio ** 2) * self.altura * 3.14
+        return volume
 
+    def __str__(self):
+        return f'Centro:({self.x_coord}, {self.y_coord}. Raio: {self.raio}. Altura: {self._altura})'
 
+class Ponto3D(Ponto):
+    __slots__ = ['_z_coord']
 
-    @capacidade.setter
-    def capacidade(self, valor):
-        self._capacidade = valor
-
-    @volume.setter
-    def volume(self, valor):
-        self._volume = valor
-
-
-    def encher(self, quantidade):
-        if not isinstance(quantidade, (int, float)):
+    def __init__(self, x_coord, y_coord, z_coord):
+        super().__init__(x_coord, y_coord)
+        if not isinstance(z_coord, (int, float)):
             raise TypeError("O valor é inválido! Tente novamente com um número válido. Ex.: 100")
-        elif quantidade < 0:
-            raise ValueError("O valor é negativo. Tente novamente com um número positivo. Ex.: 100")
-        elif quantidade + self.volume > self._capacidade:
-            raise Exception("O valor adicionado irá exceder a capacidade máxima. Tente novamente.")
         else:
-            self._volume -= quantidade
-            return self._volume
+            self._z_coord = z_coord
 
-    def despejar(self, quantidade):
-        if not isinstance(quantidade, (int, float)):
-            raise TypeError("O valor é inválido! Tente novamente com um número válido. Ex.: 100")
-        elif quantidade < 0:
-            raise ValueError("O valor é negativo. Tente novamente com um número positivo. Ex.: 100")
-        elif self._volume - quantidade < 0:
-            raise Exception("O valor adicionado será menor que zero. Tente novamente.")
-        else:
-            self._volume -= quantidade
-            return self._volume
+    @property
+    def z_coord(self):
+        return self._z_coord
+
+    @z_coord.setter
+    def z_coord(self, valor):
+        self._z_coord = valor
+
+    def distancia(self, outro_ponto):
+        #distancia = (((outro_ponto.x_coord - self.x_coord) ** 2) + ((outro_ponto.y_coord - self.y_coord) ** 2) + ((outro_ponto.z_coord - self.z_coord) ** 2)) ** 0.5
+        distancia = ((outro_ponto.x_coord - self.x_coord) ** 2) + ((outro_ponto.y_coord - self.y_coord) ** 2)
+        distancia += ((outro_ponto.z_coord - self.z_coord)) ** 2
+        distancia = distancia ** 0.5
+        return distancia
+
+    def resetar(self):
+        self.x_coord = 0
+        self.y_coord = 0
+        self.z_coord = 0
 
 
-
+    def __str__(self):
+        return f'Ponto:({self.x_coord}, {self.y_coord}, {self._z_coord})'
 
 def run():
-    try:
-        #dando erro quando é colocado algo sem ser um número
-        obj1 = Garrafa(100, 'a')
+    p1 = Ponto3D(1,2,10)
+    p2 = Ponto3D(1,2,20)
+    print(p1)
+    print(p1.distancia(p2))
 
-        #dando erro quando a  é negativa
-        obj1 = Garrafa(-1, 10)
 
-        #garrafa vazia com 500 ml de capacidade
-        obj1 = Garrafa(500, 0)
-
-        #adicionando 20 ml de água
-        print('A quantidade da volume adicionando 20 ml é: ' f'{obj1.encher(20)}')
-        # funcionando normalmente
-        print('A quantidade da volume despejando 5 ml é: ' f'{obj1.despejar(5)}')
-        #despejando mais que o possível
-        print('A quantidade da volume despejando 21 ml é: ' f'{obj1.despejar(21)}')
-        #usando número negativo
-        print('A quantidade da volume despejando -1 ml é: ' f'{obj1.despejar(-1)}')
-        #usando uma letra
-        print('A quantidade da volume despejando a ml é: ' f'{obj1.despejar("a")}')
-        #caso fosse transbordar
-        print('A quantidade da volume despejando 5000 ml é: ' f'{obj1.despejar(5000)}')
-
-    except Exception as e:
-        print(f'Erro: {e}')
-    except TypeError as e:
-        print(f'Erro: {e}')
-    except ValueError as e:
-        print(f'Erro: {e}')
 
 
 if __name__ == '__main__':
